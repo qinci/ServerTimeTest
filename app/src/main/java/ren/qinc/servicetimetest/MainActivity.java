c15b602763fb7e4f81a84e0bafdff4a0409ca215
qinci <shenqinci@qq.com>
2016-05-23 15:59:51 +0800

    说明

--- app/src/main/java/ren/qinc/servicetimetest/MainActivity.java
+++ app/src/main/java/ren/qinc/servicetimetest/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         //启动成功后获取服务器时间并初始化(这里用本机时间模拟)
         ServerTime.getInstance().initServerTime(new Date().getTime());
 
-        
+
         //以后应用所有的地方都可以使用了
        Toast.makeText(this, "服务器时间："+ServerTime.getInstance().getmServerTime(),Toast.LENGTH_SHORT).show();
     }
